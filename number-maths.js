efedfa539a9e0c1299abce0565a6ada31a98ad65
Sounalya-Kulkarni <sounalyakulakarni@gmail.com>
2026-09-02 17:32:55 +0530

    practicing numbers and string

---
+++ number-maths.js
@@ -0,0 +1,29 @@
+// for addtion
+
+var num= 10;
+num=num+5;
+console.log(num);
+
+//or
+
+var num=20;
+num+=10;
+console.log(num);
+
+var num= 2;
+num-=5;
+console.log(num);
+
+
+var num=22.22;
+num=num*2;
+console.log(num);
+
+var num=20;
+num%=10;
+console.log(num);
+
+
+var num=10;
+num=num/3;
+console.log(num);
---
+++ string.js
@@ -0,0 +1,41 @@
+
+var cityName ="bangalore";
+console.log(cityName);
+
+
+var status='checked traffic status here';
+console.log(status);
+
+
+var aboutStory=`A wise old owl sitting on a branch looked up at the sky. 
+He saw Maya down below, and then he saw the dark patch of cloud where Pip was hiding. 
+The owl flew up, high and fast, landing right on the cloud next to Pip.`
+console.log(aboutStory);
+
+var first_name="janu";
+console.log(first_name);
+
+var ComapanyName ="oracle";
+console.log(ComapanyName);
+
+
+var first_name ="ramya";
+var last_name =" rati";
+schoolName="english school";
+
+var result = first_name  +  last_name +" "+ "studied in " + schoolName
+console.log(result);
+
+
+
+var name= "basavaraj";
+
+var output=name.charAt(name.length-1);
+console.log(output);
+
+
+
+var cityName="banga_luru";
+var test=cityName.charAt(2);
+console.log(test);
+
